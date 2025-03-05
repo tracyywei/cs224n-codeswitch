1961cf0b0f03e0a1913719907fe4badd668b6384
Hannah Clay <hclay116@stanford.com>
2025-03-04 17:19:11 -0800

    step 2 finetuning - local checkpoint

--- data_augment/codeswitch_model/model.py
+++ data_augment/codeswitch_model/model.py
@@ -16,16 +16,24 @@ def finetune_mT5_codeswitched():
     dataset = CodeswitchDataset(tokenizer=tokenizer, block_size=128)
     model = MT5ForConditionalGeneration.from_pretrained("google/mt5-small")
 
+    device = torch.device("cuda")
+    print(f"Device name: {torch.cuda.get_device_name(device)}")
+    total_memory = torch.cuda.get_device_properties(device).total_memory
+    print(f"Total memory: {total_memory / (1024**3):.2f} GB")
+    allocated_memory = torch.cuda.memory_allocated(device)
+    print(f"Allocated memory: {allocated_memory / (1024**3):.2f} GB")
+    free_memory = total_memory - allocated_memory
+    print(f"Free memory: {free_memory / (1024**3):.2f} GB")
+
     tconf = TrainerConfig(
-        max_epochs = 5,          # goal range is 5-10 epochs
-        batch_size = 16,         # goal range is 8-32
+        max_epochs = 10,         # goal range is 5-10 epochs
+        batch_size = 8,          # goal range is 8-32
         learning_rate = 2e-4,    
         betas = (0.9, 0.999), 
         weight_decay = 0.01,     # avoid overregularization
         lr_decay = True,
         warmup_tokens = 1e6,
         final_tokens = 10e9,
-        ckpt_path = "./checkpoints/mT5_finetuned.pth",
         num_workers = 4, 
     )
 
@@ -44,35 +52,22 @@ def finetune_mT5_codeswitched_generation(dataset):
     '''
     STEP 2: Finetune for codeswitch generation on the parsed dataset
     '''
-    # TODO: figure out which argparse params are needed and consider hardcoding the rest
-    argp = argparse.ArgumentParser()
-    argp.add_argument("--train_file", type=str, required=True)
-    argp.add_argument("--dev_file", type=str, required=True)
-    argp.add_argument("--output_model", type=str, required=True)
-    argp.add_argument("--max_length", type=int, default=128)
-    argp.add_argument("--max_epochs", type=int, default=10)
-    argp.add_argument("--batch_size", type=int, default=32)
-    argp.add_argument("--learning_rate", type=float, default=3e-5)
-    args = argp.parse_args()
-    
-    writer = SummaryWriter(log_dir='expt/%s/%s_%s_pt_lr_%f_ft_lr_%f' % (
-      args.train_file,
-      args.dev_file,
-      args.output_model,
-      args.batch_size,
-      args.learning_rate))
-      
-    model = torch.load('mt5_intermediate_finetuned.pth')
+    model = MT5ForConditionalGeneration.from_pretrained("google/mt5-small")
+    # model.load_state_dict(torch.load('mt5_intermediate_finetuned.pth'))   # uncomment for gpu
+    model.load_state_dict(torch.load('mt5_intermediate_finetuned.pth', map_location=torch.device('cpu')))   # uncomment for cpu
+  
+    tokenizer = MT5Tokenizer.from_pretrained("google/mt5-small")
 
-    # TODO: fix trainer config to include the proper params 
+    dataset = ParsedDataset(dataset, tokenizer=tokenizer)
+
     tconf = TrainerConfig(
-        max_epochs=10,
-        batch_size=64,
-        learning_rate=3e-5,
-        
+        max_epochs=8,       # goal range is 5-10 epochs
+        batch_size=16,      # reduce from 16 to 8 if OOM occurs 
+        learning_rate=3e-4,
         lr_decay=True,
-        num_workers=0, 
-        writer=writer
+        betas = (0.9, 0.98),           
+        weight_decay = 0.01,   # Regularization to prevent overfitting
+        num_workers=0,      # change to 2 when running on gpu (0 for cpu)
     )
 
     trainer = Trainer(
@@ -88,4 +83,8 @@ def finetune_mT5_codeswitched_generation(dataset):
 
 if __name__ == '__main__':
     # run step 1
-    finetune_mT5_codeswitched()
+    # finetune_mT5_codeswitched()
+
+    # run step 2
+    dataset = "../outputs/xnli_annotated_dev.txt"
+    finetune_mT5_codeswitched_generation(dataset)
